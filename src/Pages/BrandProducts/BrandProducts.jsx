b9016fb4b433c8ea5ece2026d7966cb3ac106e6f
SaddamHossain07 <contact.saddam3@gmail.com>
2023-10-21 14:49:53 +0600

    adding products cart to the brand page

--- src/Pages/BrandProducts/BrandProducts.jsx
+++ src/Pages/BrandProducts/BrandProducts.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
+import Card from "../../components/Card/Card";
 
 const BrandProducts = () => {
     const brand = useLoaderData()
+    const { brandName } = useParams()
+    console.log(brand)
     useEffect(() => {
         fetch('http://localhost:5000/products')
             .then(res => res.json())
@@ -12,15 +15,54 @@ const BrandProducts = () => {
     }, [])
 
 
-
-
-
-
     return (
         <div>
-            {
-                brand.map(product => <p key={product._id}>{product.name} : {product.brandName}</p>)
-            }
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/3RSBHfs/10.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/VTcWSLc/11.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/ZfQz0qN/2.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/mTnNDGk/5.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                {brand.length != 0 ? <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-10/12 mx-auto mt-16">{
+                    brand?.map(product => <Card key={product._id} product={product}></Card>)}
+                </div>
+                    :
+                    <div className="text-center mt-24 space-y-6">
+                        <h3 className="text-5xl font-bold text-red-500">Opps! </h3>
+                        <p className="text-xl text-blue-600"> you have not added any product for <span className="text-2xl font-semibold">{brandName.toUpperCase()}</span> brand yet!</p>
+                        <Link to='/addProduct'>
+                            <button className="w-1/4 btn btn-primary my-10">Add Product</button>
+                        </Link>
+                    </div>
+
+                }
+            </div>
         </div>
     );
 };
