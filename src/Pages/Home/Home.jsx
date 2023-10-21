961f78302186689fffbefd777bb4e33ea9f51973
SaddamHossain07 <contact.saddam3@gmail.com>
2023-10-21 23:37:14 +0600

    completing Project

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import Brand from "../../components/Brand/Brand";
-import { useEffect, useState } from "react";
-import Card from "../../components/Card/Card";
 
 const Home = () => {
     const brands = useLoaderData()
-    const [allProducts, setAllProducts] = useState(null)
 
-    useEffect(() => {
-        fetch('https://b8a10-brandshop-server-side-saddam-hossain0743.vercel.app/products')
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setAllProducts(data)
-            }).catch(error => {
-                console.log(error)
-            })
-    }, [])
+
     return (
         <div >
             <Banner></Banner>
@@ -30,15 +18,7 @@ const Home = () => {
 
                 </div>
             </div>
-            <div className="mt-24">
-                <div>
-                    <img src="https://i.ibb.co/LvFpQ0X/banner-2.png" alt="" />
-                </div>
-                <h2 className="text-5xl font-bold logoText my-16 text-center">All Products</h2>
-                <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-10/12 mx-auto mt-16">{
-                    allProducts?.map(product => <Card key={product._id} product={product}></Card>)}
-                </div>
-            </div>
+
         </div>
     );
 };
